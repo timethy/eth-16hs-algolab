6219e1ecaf736f77de0a3f57099c8f3a7227d309
Tim Taubner <timethy@ethz.ch>
2016-12-02 20:10:10 +0100

    n nodes, m edges instead of m nodes and m² edges ;)

--- week9/casino_royale/casino-maxflow.cpp
+++ week9/casino_royale/casino-maxflow.cpp
@@ -56,54 +56,38 @@ void testcase() {
 	int n, m, l;
 	cin >> n >> m >> l;
 
-	// sorted missions (by start), start -> (end, priority)
-	// multimap<int, pair<int, int>> missions;
-	// index is x (starting station), map is from end station to priority
-	vector<vector<int>> stations(n);
-	vector<vector<int>> missions(m);
-
+	int v_source = n;
+	int max_prio = 0;
+	vector<int> xs(m), ys(m), ps(m);
 	for (int i = 0; i < m; i++) {
 		int x, y, p;
 		cin >> x >> y >> p;
-//		missions.insert(make_pair(x, make_pair(y, p)));
-		missions[i].push_back(x);
-		missions[i].push_back(y);
-		missions[i].push_back(p);
-		stations[x].push_back(i);
+		xs[i] = x;
+		ys[i] = y;
+		ps[i] = p;
 	}
 
-	int source = 2*m;
-	int source_ = 2*m+1;
-	int sink = 2*m+2;
+	max_prio = *max_element(ps.begin(), ps.end());
 
 	// Build the graph
-	Graph G(2*m+3);
+	Graph G(n+1);
     EdgeCapacityMap capacitymap = get(edge_capacity, G);
     EdgeWeightMap weightmap = get(edge_weight, G);
     ReverseEdgeMap revedgemap = get(edge_reverse, G);
 //    ResidualCapacityMap rescapacitymap = get(edge_residual_capacity, G);
-    EdgeAdder eaG(G, capacitymap, weightmap, revedgemap);
+    EdgeAdder ea(G, capacitymap, weightmap, revedgemap);
 
 	// Limit max. capacity
-	eaG.addEdge(source, source_, l, 0);
-	// Edges for each mission:
-	for(vector<vector<int>>::iterator it = missions.begin(); it != missions.end(); ++it) {
-		int i = it - missions.begin();
-		int y = (*it)[1], p = (*it)[2];
-		eaG.addEdge(source_, 2*i, 1, 0);
-		eaG.addEdge(2*i, 2*i+1, 1, -p);
-		eaG.addEdge(2*i+1, sink, 1, 0);
-		// Now add edges to all following missions:
-		for(vector<vector<int>>::iterator s_it = stations.begin() + y; s_it != stations.end(); ++s_it) {
-			vector<int> s_missions = *s_it;
-			for(vector<int>::iterator m_it = s_missions.begin(); m_it != s_missions.end(); ++m_it) {
-				int j = *m_it;
-				eaG.addEdge(2*i+1, 2*j, 1, 0);
-			}
-		}
+	ea.addEdge(v_source, 0, l, 0);
+	for (int i = 0; i < n-1; i++) {
+		ea.addEdge(i, i+1, l, 0);
 	}
+	for (int i = 0; i < m; i++) {
+		ea.addEdge(xs[i], ys[i], 1, -ps[i]);
+	}
+
 	// Maxflow-mincost
-	push_relabel_max_flow(G, source, sink);
+	push_relabel_max_flow(G, v_source, n-1);
 	cycle_canceling(G);
 	int cost = find_flow_cost(G);
 	// print out the readjusted cost (negative value)
