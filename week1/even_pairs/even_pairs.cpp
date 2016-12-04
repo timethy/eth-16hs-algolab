54074eabd35b5ff158846fbbe2310b10ecfa0287
Tim Taubner <timethy@ethz.ch>
2016-12-04 18:06:51 +0100

    MOD week1 even_pairs solved

--- week1/even_pairs/even_pairs.cpp
+++ week1/even_pairs/even_pairs.cpp
@@ -4,35 +4,39 @@
 
 using namespace std;
 
-int main (void) {
-	int t;
-	int i;
-	cin >> t;
-	for (i = 0; i < t; i++) {
-		int n;
-		cin >> n;
-		vector<int> x(n);
+void testcase() {
+	unsigned n;
+	cin >> n;
+	vector<int> x(n);
+	vector<int> even(n); // number of even sums that end in i
+	vector<int> odd(n); // number of odd sums that end in i
+	int n_pairs = 0;
 
-		for (int j = 0; j < n; j++) {
-			int x_i;
-			cin >> x_i;
-			x.push_back(x_i);
-		}
-
-		int n_pairs = 0;
-		int subset_sum = 0;
-
-		for (vector<int>::iterator it = x.begin(); it != x.end(); it++) {
-			if(*it == 0) {
-				even_length += 1;
+	for (unsigned i = 0; i < n; i++) {
+		cin >> x[i];
+		if(i == 0) {
+			if(x[i] == 0) { even[i] = 1; }
+			else { odd[i] = 1; }
+		} else {
+			if(x[i] == 0) {
+				even[i] = even[i-1]+1;
+				odd[i] = odd[i-1];
 			} else {
-				even_length = 0;
+				even[i] = odd[i-1];
+				odd[i] = even[i-1]+1;
 			}
-			n_pairs += even_length;
 		}
-
-		cout << n_pairs;
-
-		cout << "\n";
+		n_pairs += even[i];
 	}
+
+	cout << n_pairs << endl;
 }
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	int t;
+	cin >> t;
+	while(t--) { testcase(); }
+	return 0;
+}
+
