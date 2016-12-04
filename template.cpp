015da0bb63963e8ea9fff4724357aa085f1c09b1
Tim Taubner <timethy@ethz.ch>
2016-12-04 18:07:24 +0100

    ADD simple template

---
+++ template.cpp
@@ -0,0 +1,25 @@
+// for algolab, by taubnert@ethz.ch
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+// TODO: BGL
+// TODO: CGAL
+
+using namespace std;
+
+void testcase() {
+	unsigned n;
+	cin >> n;
+
+	cout << 0 << endl;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	int t;
+	cin >> t;
+	while(t--) { testcase(); }
+	return 0;
+}
