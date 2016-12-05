f398395978f89eeee2387046ee642b71609750fe
Tim Taubner <timethy@ethz.ch>
2016-12-05 15:22:39 +0100

    SOL partially week1, POTW, deck_of_cards

--- week1/deck_of_cards/deck_of_cards.cpp
+++ week1/deck_of_cards/deck_of_cards.cpp
@@ -4,16 +4,53 @@
 #include <map>
 #include <algorithm>
 
-// TODO: BGL
-// TODO: CGAL
-
 using namespace std;
 
 void testcase() {
-	unsigned n;
-	cin >> n;
+	unsigned n, k;
+	cin >> n >> k;
 
-	cout << 0 << endl;
+	vector<long> v(n);
+	vector<long> vs(n);
+	for (unsigned i = 0; i < n; i++) {
+		long v_i;
+		cin >> v_i;
+		v[i] = v_i;
+		vs[i] = v_i + ((i==0) ? 0 : vs[i-1]);
+	}
+
+	vector<long> diff(n);
+	vector<unsigned> j(n);
+	long min_diff = INT64_MAX;
+	for (long i = -1; i < n-1; i++) {
+		auto bias = i == -1 ? 0 : vs[i];
+		auto l = lower_bound(vs.begin()+i, vs.end(), k+bias);
+		auto u = upper_bound(vs.begin()+i, vs.end(), k+bias);
+		if (u == vs.end() || (l != vs.end() && k - *l <= *u - k)) {
+			diff[i+1] = k - (*l-bias);
+			j[i+1] = l - vs.begin();
+		} else {
+			diff[i+1] = (*u-bias) - k;
+			j[i+1] = u - vs.begin();
+		}
+		min_diff = min(min_diff, abs(diff[i+1]));
+	}
+	unsigned i_, j_;
+	for (unsigned i = 0; i < n; i++) {
+		int self_diff = abs(v[i] - k);
+		if (abs(self_diff) <= min_diff) {
+			i_ = i;
+			j_ = i;
+			min_diff = self_diff;
+			break;
+		} else if (min_diff == diff[i]) {
+			i_ = i;
+			j_ = j[i];
+			break;
+		}
+	}
+
+	cout << i_ << " " << j_ << endl;
 }
 
 int main() {
