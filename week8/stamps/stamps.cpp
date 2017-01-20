5409960a86aa7ef5098a710ce0baa230094fb4c4
Tim Taubner <timethy@ethz.ch>
2017-01-20 15:45:09 +0100

    ADD working solution for stamps (too slow maybe)

--- week8/stamps/stamps.cpp
+++ week8/stamps/stamps.cpp
@@ -7,11 +7,11 @@
 #include <CGAL/basic.h>
 #include <CGAL/QP_models.h>
 #include <CGAL/QP_functions.h>
-#include <CGAL/Gmpz.h>
+#include <CGAL/Gmpq.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Gmpz ET;
+typedef CGAL::Gmpq ET;
 
 using namespace std;
 
@@ -42,39 +42,47 @@ void testcase() {
 		walls[i] = Segment(start, end);
 	}
 
-	vector<unsigned> illuminated(s);
-	for (unsigned i = 0; i < l; i++) {
-		for (unsigned j = 0; j < s; j++) {
+	vector<vector<unsigned>> illuminated(s);
+	for (unsigned j = 0; j < s; j++) {
+		for (unsigned i = 0; i < l; i++) {
+			bool blocked = false;
 			for (unsigned k = 0; k < w; k++) {
 				Segment lamp_stamp = Segment(lamps[i], stamps[j]);
-				if (!CGAL::do_intersect(walls[k], lamp_stamp)) {
-					illuminated[j].push_back(i);
+				if(CGAL::do_intersect(walls[k], lamp_stamp)) {
+					blocked = true;
+					break;
 				}
+			}
+			if (!blocked) {
+				illuminated[j].push_back(i);
 			}
 		}
 	}
 
 	// Construct the linear program Ax <= b, x = [I_j]
-	Program lp(CGAL::SMALLER, true, 0, false, 0);
+	Program lp(CGAL::SMALLER, true, 1, true, 4096);
 	for (unsigned j = 0; j < s; j++) {
+//		cout << illuminated[j].size() << endl;
 		for (auto it = illuminated[j].begin(); it != illuminated[j].end(); ++it) {
 			unsigned i = *it;
 			auto r2 = CGAL::squared_distance(lamps[i], stamps[j]);
-			lp.set_a(i, j, 1/ET(r2));
+			lp.set_a(i, 2*j,   1/ET(r2));
+			lp.set_a(i, 2*j+1, 1/ET(r2));
 		}
-		lp.set_b(j, M[j]);
-		// Constraint I_j >= 1
-		lp.set_l(j, true, 1);
+		lp.set_b(2*j,   M[j]);
+		lp.set_b(2*j+1, 1);
+		lp.set_r(2*j+1, CGAL::LARGER);
+
+		lp.set_c(j, 1);
 	}
 
 	Solution sol = CGAL::solve_linear_program(lp, ET());
+//	cout << sol << endl;
 	if(sol.is_infeasible()) {
 		cout << "no" << endl;
 	} else {
 		cout << "yes" << endl;
 	}
-
-	cout << 0 << endl;
 }
 
 int main() {
