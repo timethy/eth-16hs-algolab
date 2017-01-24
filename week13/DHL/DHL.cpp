bc0c67301536668fdd058cee2add79b78f05eb5c
Tim Taubner <timethy@ethz.ch>
2017-01-24 08:59:28 +0100

    MOD solution week13/DHL to use memo (actually slower than DP)

--- week13/DHL/DHL.cpp
+++ week13/DHL/DHL.cpp
@@ -8,6 +8,57 @@
 
 using namespace std;
 
+typedef vector <vector<int>> vvec;
+
+vvec MM;
+
+int memo(vector<int> &Sa, vector<int> &Sb, unsigned i, unsigned j) {
+	if (i == 0 || j == 0) {
+		if (i == 0 && j == 0) {
+			return 0;
+		} else {
+			return INT_MAX/2;
+		}
+	} else if (MM[i][j] >= 0) {
+		return MM[i][j];
+	} else {
+		// Take one from left
+		int min_cost = INT_MAX;
+		{
+			unsigned j_ = j - 1;
+			int Sb_ = Sb[j - 1];
+			if (j_ > 0) { Sb_ -= Sb[j_ - 1]; }
+			int fb = Sb_ - (j - j_);
+			for (unsigned i_ = 0; i_ < i; i_++) {
+				// Make sure, not one stack is empty at the end (without the other being empty)
+				if ((i_ == 0 && j_ == 0) || (i_ != 0 && j_ != 0)) {
+					int Sa_ = Sa[i - 1];
+					if (i_ > 0) { Sa_ -= Sa[i_ - 1]; }
+					int cost = memo(Sa, Sb, i_, j_) + (Sa_ - (i - i_))*fb;
+					min_cost = min(min_cost, cost);
+				}
+			}
+		}
+		{
+			unsigned i_ = i - 1;
+			int Sa_ = Sa[i - 1];
+			if (i_ > 0) { Sa_ -= Sa[i_ - 1]; }
+			int fa = Sa_ - (i - i_);
+			for (unsigned j_ = 0; j_ < j; j_++) {
+				// Make sure, not one stack is empty at the end (without the other being empty)
+				if ((i_ == 0 && j_ == 0) || (i_ != 0 && j_ != 0)) {
+					int Sb_ = Sb[j - 1];
+					if (j_ > 0) { Sb_ -= Sb[j_ - 1]; }
+					int cost = memo(Sa, Sb, i_, j_) + fa*(Sb_ - (j - j_));
+					min_cost = min(min_cost, cost);
+				}
+			}
+		}
+		MM[i][j] = min_cost;
+		return min_cost;
+	}
+}
+
 void testcase() {
 	unsigned n;
 	cin >> n;
@@ -29,45 +80,8 @@ void testcase() {
 		Sb[i] = Sb[i - 1] + b;
 	}
 
-	vector <vector<int>> DP(n + 1, vector<int>(n+1));
-	for (unsigned i = 1; i <= n; i++) {
-		for (unsigned j = 1; j <= n; j++) {
-			int min_cost = INT_MAX;
-			{
-				unsigned j_ = j - 1;
-				int Sb_ = Sb[j - 1];
-				if (j_ > 0) { Sb_ -= Sb[j_ - 1]; }
-				int fb = Sb_ - (j - j_);
-				for (unsigned i_ = 0; i_ < i; i_++) {
-					// Make sure, not one stack is empty at the end (without the other being empty)
-					if ((i_ == 0 && j_ == 0) || (i_ != 0 && j_ != 0)) {
-						int Sa_ = Sa[i - 1];
-						if (i_ > 0) { Sa_ -= Sa[i_ - 1]; }
-						int cost = DP[i_][j_] + (Sa_ - (i - i_))*fb;
-						min_cost = min(min_cost, cost);
-					}
-				}
-			}
-			{
-				unsigned i_ = i - 1;
-				int Sa_ = Sa[i - 1];
-				if (i_ > 0) { Sa_ -= Sa[i_ - 1]; }
-				int fa = Sa_ - (i - i_);
-				for (unsigned j_ = 0; j_ < j; j_++) {
-					// Make sure, not one stack is empty at the end (without the other being empty)
-					if ((i_ == 0 && j_ == 0) || (i_ != 0 && j_ != 0)) {
-						int Sb_ = Sb[j - 1];
-						if (j_ > 0) { Sb_ -= Sb[j_ - 1]; }
-						int cost = DP[i_][j_] + fa*(Sb_ - (j - j_));
-						min_cost = min(min_cost, cost);
-					}
-				}
-			}
-			DP[i][j] = min_cost;
-		}
-	}
-
-	cout << DP[n][n] << endl;
+	MM = vvec(n + 1, vector<int>(n + 1, -1));
+	cout << memo(Sa, Sb, n, n) << endl;
 }
 
 int main() {
